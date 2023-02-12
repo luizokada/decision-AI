404fbe29a18c7871b7f9795db27acd5d57a908db
luizokada <luizfernandookada@gmail.com>
2023-02-12 18:45:27 -0300

    fix: variables names and add: decisions utils

--- assets/references.py
+++ assets/references.py
@@ -5,9 +5,9 @@ __ibm_groups = {
         }
 
 __glucose_groups = {
-            "Class 1": (0.0, 140),
-            "Class 2": (140, 200),
-            "Class 3": (200, 1000),
+            "Class 1": (0.0, 100),
+            "Class 2": (100, 125),
+            "Class 3": (125, 100000),
         }
 
 __insulin_groups = {
@@ -21,10 +21,57 @@ __pedigree_groups = {
             "Class 2": (0.3, 10),
         }
 
+_helthy_util={
+            "output":"Não precisa se preucupar",
+            "BMI 1":25,
+            "BMI 2":60,
+            "BMI 3":25,
+            "Glucose 1":50,
+            "Glucose 2":30,
+            "Glucose 3":15,
+            "Insulin 1":80,
+            "Insulin 2":15,
+            "Insulin 3":0,
+            "DiabetesPedigreeFunction 1":45,
+            "DiabetesPedigreeFunction 2":25
+        }
+        
+_should_go_on_diet_util={
+            "output":"Deve melhorar a alimentação",
+            "BMI 1":55,
+            "BMI 2":25,
+            "BMI 3":65,
+            "Glucose 1":30,
+            "Glucose 2":70,
+            "Glucose 3":25,
+            "Insulin 1":20,
+            "Insulin 2":70,
+            "Insulin 3":0,
+            "DiabetesPedigreeFunction 1":30,
+            "DiabetesPedigreeFunction 2":50
+        }
+_need_to_go_on_doctor_util={
+            "output":"Deve ir ao médico",
+            "BMI 1":75,
+            "BMI 2":15,
+            "BMI 3":75,
+            "Glucose 1":25,
+            "Glucose 2":60,
+            "Glucose 3":90,
+            "Insulin 1":30,
+            "Insulin 2":50,
+            "Insulin 3":100,
+            "DiabetesPedigreeFunction 1":15,
+            "DiabetesPedigreeFunction 2":70
+        }
+
 references = {
-    "ibm": __ibm_groups,
-    "glucose": __glucose_groups,
-    "insulin": __insulin_groups,
-    "pedigree": __pedigree_groups,
+    "BMI": __ibm_groups,
+    "Glucose": __glucose_groups,
+    "Insulin": __insulin_groups,
+    "DiabetesPedigreeFunction": __pedigree_groups,
+    "helthy_decision": _helthy_util,
+    "should_go_on_diet_decision": _should_go_on_diet_util,
+    "need_to_go_on_doctor_decision": _need_to_go_on_doctor_util,
 }
 
