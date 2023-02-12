5e884e266fd369f224053599a0c5c2c225e93246
luizokada <luizfernandookada@gmail.com>
2023-02-11 23:33:22 -0300

    fix: conditions

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import pandas as pd
 
 db = pd.read_csv('./DB/diabetes.csv')
 probHasDiabetesDict = {
-    "Age":[],
+    "BMI":[],
     "Glucose":[],
     "Insulin":[],
     "DiabetesPedigreeFunction":[],
@@ -12,7 +12,7 @@ probHasDiabetesDict = {
 }
 
 probHasNotDiabetesDict = {
-    "Age":[],
+    "BMI":[],
     "Glucose":[],
     "Insulin":[],
     "DiabetesPedigreeFunction":[],
@@ -23,41 +23,41 @@ conditonHasDiabetes = db["Outcome"] == 1
 num_total_diabetes = db.loc[conditonHasDiabetes].shape[0]
 num_not_diabetes = num_total - num_total_diabetes
 
-print("Ages classes")
+print("BMIs classes")
 print("Class 1: 0 - 30")
 print("Class 2: 30 - 50")
 print("Class 3: >50")
 print()
-conditionAgeClass1 = (db["Outcome"]== 1 ) & (db["Age"] <= 30)
-conditionAgeClass2 = (db["Outcome"]== 1 ) & (db["Age"] >30) &(db["Age"] <= 50)
-conditionAgeClass3 = (db["Outcome"]== 1 ) & (db["Age"] >50)
+conditionBMIClass1 = (db["Outcome"]== 1 ) & (db["BMI"] <= 18.5)
+conditionBMIClass2 = (db["Outcome"]== 1 ) & (db["BMI"] >18.5) &(db["BMI"] <= 25)
+conditionBMIClass3 = (db["Outcome"]== 1 ) & (db["BMI"] >25)
 
 print("Outcome == 1 ")
-print("Age Class 1: ", db.loc[conditionAgeClass1].shape[0])
-print("Age Class 2: ", db.loc[conditionAgeClass2].shape[0])
-print("Age Class 3: ", db.loc[conditionAgeClass3].shape[0])
+print("BMI Class 1: ", db.loc[conditionBMIClass1].shape[0])
+print("BMI Class 2: ", db.loc[conditionBMIClass2].shape[0])
+print("BMI Class 3: ", db.loc[conditionBMIClass3].shape[0])
 
-probHasDiabetesDict["Age"].append(db.loc[conditionAgeClass1].shape[0]/num_total)
-probHasDiabetesDict["Age"].append(db.loc[conditionAgeClass2].shape[0]/num_total)
-probHasDiabetesDict["Age"].append(db.loc[conditionAgeClass3].shape[0]/num_total)
+probHasDiabetesDict["BMI"].append(db.loc[conditionBMIClass1].shape[0]/num_total)
+probHasDiabetesDict["BMI"].append(db.loc[conditionBMIClass2].shape[0]/num_total)
+probHasDiabetesDict["BMI"].append(db.loc[conditionBMIClass3].shape[0]/num_total)
 
 print()
-conditionAgeClass1 = (db["Outcome"]== 0 ) & (db["Age"] <= 30)
-conditionAgeClass2 = (db["Outcome"]== 0 ) & (db["Age"] >30) &(db["Age"] <= 50)
-conditionAgeClass3 = (db["Outcome"]== 0 ) & (db["Age"] > 50)
+conditionBMIClass1 = (db["Outcome"]== 0 ) & (db["BMI"] <= 18.5)
+conditionBMIClass2 = (db["Outcome"]== 0 ) & (db["BMI"] >18.5) &(db["BMI"] <= 25)
+conditionBMIClass3 = (db["Outcome"]== 0 ) & (db["BMI"] > 25)
 
 
 
 
 print("Outcome == 0 ")
-print("Age Class 1: ", db.loc[conditionAgeClass1].shape[0])
-print("Age Class 2: ", db.loc[conditionAgeClass2].shape[0])
-print("Age Class 3: ", db.loc[conditionAgeClass3].shape[0])
+print("BMI Class 1: ", db.loc[conditionBMIClass1].shape[0])
+print("BMI Class 2: ", db.loc[conditionBMIClass2].shape[0])
+print("BMI Class 3: ", db.loc[conditionBMIClass3].shape[0])
 
 
-probHasNotDiabetesDict["Age"].append(db.loc[conditionAgeClass1].shape[0]/num_total)
-probHasNotDiabetesDict["Age"].append(db.loc[conditionAgeClass2].shape[0]/num_total)
-probHasNotDiabetesDict["Age"].append(db.loc[conditionAgeClass3].shape[0]/num_total)
+probHasNotDiabetesDict["BMI"].append(db.loc[conditionBMIClass1].shape[0]/num_total)
+probHasNotDiabetesDict["BMI"].append(db.loc[conditionBMIClass2].shape[0]/num_total)
+probHasNotDiabetesDict["BMI"].append(db.loc[conditionBMIClass3].shape[0]/num_total)
 
 print()
 
@@ -67,9 +67,9 @@ print("Class 2: 100 - 150")
 print("Class 3: >150")
 print()
 
-conditionGlucoseClass1 = (db["Outcome"]== 1 ) & (db["Glucose"] < 100)
-conditionGlucoseClass2 = (db["Outcome"]== 1 ) & (db["Glucose"] > 100) &(db["Glucose"] <= 150)
-conditionGlucoseClass3 = (db["Outcome"]== 1 ) & (db["Glucose"] > 150)
+conditionGlucoseClass1 = (db["Outcome"]== 1 ) & (db["Glucose"] <= 100)
+conditionGlucoseClass2 = (db["Outcome"]== 1 ) & (db["Glucose"] > 100) &(db["Glucose"] <= 125)
+conditionGlucoseClass3 = (db["Outcome"]== 1 ) & (db["Glucose"] > 125)
 
 print("Outcome == 1 ")
 print("Glucose Class 1: ", db.loc[conditionGlucoseClass1].shape[0])
@@ -82,9 +82,9 @@ probHasDiabetesDict["Glucose"].append(db.loc[conditionGlucoseClass3].shape[0]/nu
 
 
 print()
-conditionGlucoseClass1 = (db["Outcome"]== 0 ) & (db["Glucose"] < 100)
-conditionGlucoseClass2 = (db["Outcome"]== 0 ) & (db["Glucose"] > 100) &(db["Glucose"] <= 150)
-conditionGlucoseClass3 = (db["Outcome"]== 0 ) & (db["Glucose"] > 150)
+conditionGlucoseClass1 = (db["Outcome"]== 0 ) & (db["Glucose"] <= 100)
+conditionGlucoseClass2 = (db["Outcome"]== 0 ) & (db["Glucose"] > 100) &(db["Glucose"] <= 125)
+conditionGlucoseClass3 = (db["Outcome"]== 0 ) & (db["Glucose"] > 125)
 
 print("Outcome == 0 ")
 print("Glucose Class 1: ", db.loc[conditionGlucoseClass1].shape[0])
@@ -102,9 +102,9 @@ print("Class 1: 0")
 print("Class 2: 0 - 100")
 print("Class 3: >100")
 print()
-conditionInsulinClass1 = (db["Outcome"]== 1 ) & (db["Insulin"] == 0)
-conditionInsulinClass2 = (db["Outcome"]== 1 ) & (db["Insulin"] > 0) &(db["Insulin"] <= 100)
-conditionInsulinCLass3 = (db["Outcome"]== 1 ) & (db["Insulin"] > 100)
+conditionInsulinClass1 = (db["Outcome"]== 1 ) & (db["Insulin"] <= 140)
+conditionInsulinClass2 = (db["Outcome"]== 1 ) & (db["Insulin"] > 140) &(db["Insulin"] <= 199)
+conditionInsulinCLass3 = (db["Outcome"]== 1 ) & (db["Insulin"] > 199)
 
 print("Outcome == 1 ")
 print("Insulin Class 1: ", db.loc[conditionInsulinClass1].shape[0])
@@ -117,9 +117,9 @@ probHasDiabetesDict["Insulin"].append(db.loc[conditionInsulinCLass3].shape[0]/nu
 
 print()
 
-conditionInsulinClass1 = (db["Outcome"]== 0 ) & (db["Insulin"] == 0)
-conditionInsulinClass2 = (db["Outcome"]== 0 ) & (db["Insulin"] > 0) &(db["Insulin"] <= 100)
-conditionInsulinCLass3 = (db["Outcome"]== 0 ) & (db["Insulin"] > 100)
+conditionInsulinClass1 = (db["Outcome"]== 0 ) & (db["Insulin"] <= 140)
+conditionInsulinClass2 = (db["Outcome"]== 0 ) & (db["Insulin"] > 140) &(db["Insulin"] <= 199)
+conditionInsulinCLass3 = (db["Outcome"]== 0 ) & (db["Insulin"] > 199)
 
 print("Outcome == 0 ")
 print("Insulin Class 1: ", db.loc[conditionInsulinClass1].shape[0])
