974c2cf4e1f71024420828384ded550ce8b22f15
beavanzi <bia.avanzi@gmail.com>
2023-02-11 21:04:03 -0300

    formatting case adjusted

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import pandas as pd
 
 
 db = pd.read_csv('./DB/diabetes.csv')
-probHasDiabetesDict = {
+
+prob_has_diabetes = {
     "Age":[],
     "Glucose":[],
     "Insulin":[],
@@ -11,16 +12,16 @@ probHasDiabetesDict = {
     
 }
 
-probHasNotDiabetesDict = {
+prob_has_not_diabetes = {
     "Age":[],
     "Glucose":[],
     "Insulin":[],
     "DiabetesPedigreeFunction":[],
-    
 }
+
 num_total = db.shape[0]
-conditonHasDiabetes = db["Outcome"] == 1
-num_total_diabetes = db.loc[conditonHasDiabetes].shape[0]
+cond_diabetes = db["Outcome"] == 1
+num_total_diabetes = db.loc[cond_diabetes].shape[0]
 num_not_diabetes = num_total - num_total_diabetes
 
 print("Ages classes")
@@ -28,36 +29,35 @@ print("Class 1: 0 - 30")
 print("Class 2: 30 - 50")
 print("Class 3: >50")
 print()
-conditionAgeClass1 = (db["Outcome"]== 1 ) & (db["Age"] <= 30)
-conditionAgeClass2 = (db["Outcome"]== 1 ) & (db["Age"] >30) &(db["Age"] <= 50)
-conditionAgeClass3 = (db["Outcome"]== 1 ) & (db["Age"] >50)
+
+cond_age_1 = cond_diabetes & (db["Age"] <= 30)
+cond_age_2 = cond_diabetes & (db["Age"] >30) &(db["Age"] <= 50)
+cond_age_3 = cond_diabetes & (db["Age"] >50)
 
 print("Outcome == 1 ")
-print("Age Class 1: ", db.loc[conditionAgeClass1].shape[0])
-print("Age Class 2: ", db.loc[conditionAgeClass2].shape[0])
-print("Age Class 3: ", db.loc[conditionAgeClass3].shape[0])
+print("Age Class 1: ", db.loc[cond_age_1].shape[0])
+print("Age Class 2: ", db.loc[cond_age_2].shape[0])
+print("Age Class 3: ", db.loc[cond_age_3].shape[0])
 
-probHasDiabetesDict["Age"].append(db.loc[conditionAgeClass1].shape[0]/num_total)
-probHasDiabetesDict["Age"].append(db.loc[conditionAgeClass2].shape[0]/num_total)
-probHasDiabetesDict["Age"].append(db.loc[conditionAgeClass3].shape[0]/num_total)
+prob_has_diabetes["Age"].append(db.loc[cond_age_1].shape[0]/num_total)
+prob_has_diabetes["Age"].append(db.loc[cond_age_2].shape[0]/num_total)
+prob_has_diabetes["Age"].append(db.loc[cond_age_3].shape[0]/num_total)
 
 print()
-conditionAgeClass1 = (db["Outcome"]== 0 ) & (db["Age"] <= 30)
-conditionAgeClass2 = (db["Outcome"]== 0 ) & (db["Age"] >30) &(db["Age"] <= 50)
-conditionAgeClass3 = (db["Outcome"]== 0 ) & (db["Age"] > 50)
-
-
+cond_age_1 = (db["Outcome"]== 0 ) & (db["Age"] <= 30)
+cond_age_2 = (db["Outcome"]== 0 ) & (db["Age"] >30) &(db["Age"] <= 50)
+cond_age_3 = (db["Outcome"]== 0 ) & (db["Age"] >50)
 
 
 print("Outcome == 0 ")
-print("Age Class 1: ", db.loc[conditionAgeClass1].shape[0])
-print("Age Class 2: ", db.loc[conditionAgeClass2].shape[0])
-print("Age Class 3: ", db.loc[conditionAgeClass3].shape[0])
+print("Age Class 1: ", db.loc[cond_age_1].shape[0])
+print("Age Class 2: ", db.loc[cond_age_2].shape[0])
+print("Age Class 3: ", db.loc[cond_age_3].shape[0])
 
 
-probHasNotDiabetesDict["Age"].append(db.loc[conditionAgeClass1].shape[0]/num_total)
-probHasNotDiabetesDict["Age"].append(db.loc[conditionAgeClass2].shape[0]/num_total)
-probHasNotDiabetesDict["Age"].append(db.loc[conditionAgeClass3].shape[0]/num_total)
+prob_has_not_diabetes["Age"].append(db.loc[cond_age_1].shape[0]/num_total)
+prob_has_not_diabetes["Age"].append(db.loc[cond_age_2].shape[0]/num_total)
+prob_has_not_diabetes["Age"].append(db.loc[cond_age_3].shape[0]/num_total)
 
 print()
 
@@ -67,33 +67,33 @@ print("Class 2: 100 - 150")
 print("Class 3: >150")
 print()
 
-conditionGlucoseClass1 = (db["Outcome"]== 1 ) & (db["Glucose"] < 100)
-conditionGlucoseClass2 = (db["Outcome"]== 1 ) & (db["Glucose"] > 100) &(db["Glucose"] <= 150)
-conditionGlucoseClass3 = (db["Outcome"]== 1 ) & (db["Glucose"] > 150)
+cond_glucose_1 = (db["Outcome"]== 1 ) & (db["Glucose"] < 100)
+cond_glucose_2 = (db["Outcome"]== 1 ) & (db["Glucose"] > 100) &(db["Glucose"] <= 150)
+cond_glucose_3 = (db["Outcome"]== 1 ) & (db["Glucose"] > 150)
 
 print("Outcome == 1 ")
-print("Glucose Class 1: ", db.loc[conditionGlucoseClass1].shape[0])
-print("Glucose Class 2: ", db.loc[conditionGlucoseClass2].shape[0])
-print("Glucose Class 3: ", db.loc[conditionGlucoseClass3].shape[0])
+print("Glucose Class 1: ", db.loc[cond_glucose_1].shape[0])
+print("Glucose Class 2: ", db.loc[cond_glucose_2].shape[0])
+print("Glucose Class 3: ", db.loc[cond_glucose_3].shape[0])
 
-probHasDiabetesDict["Glucose"].append(db.loc[conditionGlucoseClass1].shape[0]/num_total)
-probHasDiabetesDict["Glucose"].append(db.loc[conditionGlucoseClass2].shape[0]/num_total)
-probHasDiabetesDict["Glucose"].append(db.loc[conditionGlucoseClass3].shape[0]/num_total)
+prob_has_diabetes["Glucose"].append(db.loc[cond_glucose_1].shape[0]/num_total)
+prob_has_diabetes["Glucose"].append(db.loc[cond_glucose_2].shape[0]/num_total)
+prob_has_diabetes["Glucose"].append(db.loc[cond_glucose_3].shape[0]/num_total)
 
 
 print()
-conditionGlucoseClass1 = (db["Outcome"]== 0 ) & (db["Glucose"] < 100)
-conditionGlucoseClass2 = (db["Outcome"]== 0 ) & (db["Glucose"] > 100) &(db["Glucose"] <= 150)
-conditionGlucoseClass3 = (db["Outcome"]== 0 ) & (db["Glucose"] > 150)
+cond_glucose_1 = (db["Outcome"]== 0 ) & (db["Glucose"] < 100)
+cond_glucose_2 = (db["Outcome"]== 0 ) & (db["Glucose"] > 100) &(db["Glucose"] <= 150)
+cond_glucose_3 = (db["Outcome"]== 0 ) & (db["Glucose"] > 150)
 
 print("Outcome == 0 ")
-print("Glucose Class 1: ", db.loc[conditionGlucoseClass1].shape[0])
-print("Glucose Class 2: ", db.loc[conditionGlucoseClass2].shape[0])
-print("Glucose Class 3: ", db.loc[conditionGlucoseClass3].shape[0])
+print("Glucose Class 1: ", db.loc[cond_glucose_1].shape[0])
+print("Glucose Class 2: ", db.loc[cond_glucose_2].shape[0])
+print("Glucose Class 3: ", db.loc[cond_glucose_3].shape[0])
 
-probHasNotDiabetesDict["Glucose"].append(db.loc[conditionGlucoseClass1].shape[0]/num_total)
-probHasNotDiabetesDict["Glucose"].append(db.loc[conditionGlucoseClass2].shape[0]/num_total)
-probHasNotDiabetesDict["Glucose"].append(db.loc[conditionGlucoseClass3].shape[0]/num_total)
+prob_has_not_diabetes["Glucose"].append(db.loc[cond_glucose_1].shape[0]/num_total)
+prob_has_not_diabetes["Glucose"].append(db.loc[cond_glucose_2].shape[0]/num_total)
+prob_has_not_diabetes["Glucose"].append(db.loc[cond_glucose_3].shape[0]/num_total)
 
 print()
 
@@ -111,9 +111,9 @@ print("Insulin Class 1: ", db.loc[conditionInsulinClass1].shape[0])
 print("Insulin Class 2: ", db.loc[conditionInsulinClass2].shape[0])
 print("Insulin Class 3: ", db.loc[conditionInsulinCLass3].shape[0])
 
-probHasDiabetesDict["Insulin"].append(db.loc[conditionInsulinClass1].shape[0]/num_total)
-probHasDiabetesDict["Insulin"].append(db.loc[conditionInsulinClass2].shape[0]/num_total)
-probHasDiabetesDict["Insulin"].append(db.loc[conditionInsulinCLass3].shape[0]/num_total)
+prob_has_diabetes["Insulin"].append(db.loc[conditionInsulinClass1].shape[0]/num_total)
+prob_has_diabetes["Insulin"].append(db.loc[conditionInsulinClass2].shape[0]/num_total)
+prob_has_diabetes["Insulin"].append(db.loc[conditionInsulinCLass3].shape[0]/num_total)
 
 print()
 
@@ -127,9 +127,9 @@ print("Insulin Class 2: ", db.loc[conditionInsulinClass2].shape[0])
 print("Insulin Class 3: ", db.loc[conditionInsulinCLass3].shape[0])
 
 
-probHasNotDiabetesDict["Insulin"].append(db.loc[conditionInsulinClass1].shape[0]/num_total)
-probHasNotDiabetesDict["Insulin"].append(db.loc[conditionInsulinClass2].shape[0]/num_total)
-probHasNotDiabetesDict["Insulin"].append(db.loc[conditionInsulinCLass3].shape[0]/num_total)
+prob_has_not_diabetes["Insulin"].append(db.loc[conditionInsulinClass1].shape[0]/num_total)
+prob_has_not_diabetes["Insulin"].append(db.loc[conditionInsulinClass2].shape[0]/num_total)
+prob_has_not_diabetes["Insulin"].append(db.loc[conditionInsulinCLass3].shape[0]/num_total)
 print()
 
 print("DiabetesPedigreeFunction classes")
@@ -137,46 +137,46 @@ print("Class 1: <= 0.3")
 print("Class 2: > 0.3")
 print()
 
-conditionDiaPedigreeClass1 = (db["Outcome"]== 1 ) & (db["DiabetesPedigreeFunction"] <= 0.3)
-conditionDiaPedigreeClass2 = (db["Outcome"]== 1 ) & (db["DiabetesPedigreeFunction"] > 0.3) 
+cond_diab_pedigree_1 = (db["Outcome"]== 1 ) & (db["DiabetesPedigreeFunction"] <= 0.3)
+cond_diab_pedigree_2 = (db["Outcome"]== 1 ) & (db["DiabetesPedigreeFunction"] > 0.3)
 
 print("Outcome == 1 ")
-print("DiabetesPedigreeFunction Class 1: ", db.loc[conditionDiaPedigreeClass1].shape[0])
-print("DiabetesPedigreeFunction Class 2: ", db.loc[conditionDiaPedigreeClass2].shape[0])
+print("DiabetesPedigreeFunction Class 1: ", db.loc[cond_diab_pedigree_1].shape[0])
+print("DiabetesPedigreeFunction Class 2: ", db.loc[cond_diab_pedigree_2].shape[0])
 
-probHasDiabetesDict["DiabetesPedigreeFunction"].append(db.loc[conditionDiaPedigreeClass1].shape[0]/num_total)
-probHasDiabetesDict["DiabetesPedigreeFunction"].append(db.loc[conditionDiaPedigreeClass2].shape[0]/num_total)
+prob_has_diabetes["DiabetesPedigreeFunction"].append(db.loc[cond_diab_pedigree_1].shape[0]/num_total)
+prob_has_diabetes["DiabetesPedigreeFunction"].append(db.loc[cond_diab_pedigree_2].shape[0]/num_total)
 
 print()
 
-conditionDiaPedigreeClass1 = (db["Outcome"]== 0 ) & (db["DiabetesPedigreeFunction"] <= 0.3)
-conditionDiaPedigreeClass2 = (db["Outcome"]== 0 ) & (db["DiabetesPedigreeFunction"] > 0.3) 
+cond_diab_pedigree = (db["Outcome"]== 0 ) & (db["DiabetesPedigreeFunction"] <= 0.3)
+cond_diab_pedigree_2 = (db["Outcome"]== 0 ) & (db["DiabetesPedigreeFunction"] > 0.3) 
 
 print("Outcome == 0 ")
-print("DiabetesPedigreeFunction Class 1: ", db.loc[conditionDiaPedigreeClass1].shape[0])
-print("DiabetesPedigreeFunction Class 2: ", db.loc[conditionDiaPedigreeClass2].shape[0])
+print("DiabetesPedigreeFunction Class 1: ", db.loc[cond_diab_pedigree].shape[0])
+print("DiabetesPedigreeFunction Class 2: ", db.loc[cond_diab_pedigree_2].shape[0])
 
 
-probHasNotDiabetesDict["DiabetesPedigreeFunction"].append(db.loc[conditionDiaPedigreeClass1].shape[0]/num_total)
-probHasNotDiabetesDict["DiabetesPedigreeFunction"].append(db.loc[conditionDiaPedigreeClass2].shape[0]/num_total)
+prob_has_not_diabetes["DiabetesPedigreeFunction"].append(db.loc[cond_diab_pedigree].shape[0]/num_total)
+prob_has_not_diabetes["DiabetesPedigreeFunction"].append(db.loc[cond_diab_pedigree_2].shape[0]/num_total)
 
 print()
 
 print("P(key)  outcome ==1")
 print()
 
-for key in probHasDiabetesDict:
+for key in prob_has_diabetes:
     print(key, ":")
     print()
-    for i in range(len(probHasDiabetesDict[key])):
-        print("Class ", i+1, ": ", probHasDiabetesDict[key][i])
+    for i in range(len(prob_has_diabetes[key])):
+        print("Class ", i+1, ": ", prob_has_diabetes[key][i])
     print()
         
 print("P(key)  outcome ==0")
 
-for key in probHasNotDiabetesDict:
+for key in prob_has_not_diabetes:
     print(key, ":")
-    for i in range(len(probHasNotDiabetesDict[key])):
-        print("Class ", i+1, ": ", probHasNotDiabetesDict[key][i])
+    for i in range(len(prob_has_not_diabetes[key])):
+        print("Class ", i+1, ": ", prob_has_not_diabetes[key][i])
     print()
 
